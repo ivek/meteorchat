f8547219d4ca133abab8dcc6536a16a0d2c8b763
ivek <ivekdelfin@gmail.com>
2017-05-26 14:01:17 -0500

    first step

---
+++ client/main.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Meteor} from 'meteor/meteor'
+
+class Comment extends React.Component{
+  constructor(){
+    super()
+  }
+  render(){
+    return (<h4>{this.props.comment}</h4>)
+  }
+}
+
+class App extends React.Component{
+  constructor(){
+    super()
+  }
+  render(){
+    let comments=[
+      {_id: new Date(), comment: "hola meteor"},
+      {_id: new Date()+1, comment: "hola meteor"},
+      {_id: new Date()+2, comment: "hola ivek"}
+    ]
+    let renderComments=comments.map((comment)=>{
+      return(<Comment key={comment._id} comment={comment.comment}/>)
+    })
+    return(<div>{renderComments}</div>)
+  }
+}
+Meteor.startup(()=>{
+  render(<App/>, document.getElementById('container'))
+})
